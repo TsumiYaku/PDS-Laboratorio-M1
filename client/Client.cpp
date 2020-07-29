9b5775f177369aa579bfefb80270685c2181e043
s277922 <s277922@studenti.polito.it>
2020-07-29 12:18:03 +0200

    .

--- client/Client.cpp
+++ client/Client.cpp
@@ -7,8 +7,12 @@
 #include <string>
 #include <list>
 #include <map>
+#include <netinet/in.h>
 #include <functional>
+#include <sys/stat.h>
 #include <shared_mutex>
+#include <sys/sendfile.h>
+#include <fcntl.h>
 #include <string.h>
 #include <sstream>
 #include <optional>
@@ -58,34 +62,100 @@ void Client::handleConnection(){
             std::string directory = this->readline();
             this->status = active;
             path dir(directory);
+            sock = socket(AF_INET, SOCK_STREAM, 0);
+            if(sock == -1)
+            {
+               log("socket creation failed");
+               return;
+            }
+            //saddr.sin_family = AF_INET;
+            //saddr.sin_port = ntohs(server->port);
+
             if(exists(dir) && is_directory(dir)){
                 if(/*server->findDirectory(directory, user)*/ true){
-                    //TO DO: ho trovato la cartella sul server per quel user e verifico se combacia 
-                    //con quella del client
+                    //TO DO eseguo checksum su le due directory
                 }else{
-                    //TO DO: invierò il contenuto dei vari file e cartelle al server
-                    //per potermi sincronizzare e creare la cartella lato server
+                    path dir(directory);
+                    for (auto& p : recursive_directory_iterator(dir)) {
+                         if(is_directory(p)){//invio il nome della cartella al server
+                           send(sock, p.path().relative_path().c_str(), strlen(p.path().relative_path().c_str())+1, 0);
+                           recv(sock, &status, sizeof(int), 0);
+                           if(status)
+                                log("OK");
+                           else
+                                throw std::runtime_error("error");
+                         }
+                         else if(is_regular_file(p)) //invio il file e il suo contenuto
+                         {
+                           struct stat obj;
+                           int size, filehandle;
+                           filehandle = open(p.path().relative_path().c_str(), O_RDONLY);
+                           if(filehandle==-1){
+                                 throw std::runtime_error("eimpossibile aprire file");
+                           }else{
+                                //invio nome file
+                                send(sock, p.path().relative_path().c_str(), strlen(p.path().c_str())+1, 0);
+                                //calcolo la sua dimensione e la invio al server
+                                stat(p.path().c_str(), &obj); 
+                                size = obj.st_size;
+                                send(sock, &size, sizeof(int), 0);
+                                //invio file
+                                sendfile(sock, filehandle, NULL, size);
+                                //ricevo un ack da server se è andato tutto a buon fine
+                                recv(sock, &status, sizeof(int), 0);
+                                if(status)
+                                    log("OK");
+                                else
+                                   throw std::runtime_error("error");
+                           }
+                         }
+                    }
                 }
             }
             else if(!exists(dir)){
-                //TO DO: errore, creo cartella
+                path p(directory);
+                if(!create_directory(p))
+                   throw std::runtime_error("impossibile creare directory");
+                else
+                {
+                    send(sock, p.relative_path().c_str(), strlen(p.relative_path().c_str())+1, 0);
+                    recv(sock, &status, sizeof(int), 0);
+                    if(status)
+                        log("OK");
+                    else
+                        throw std::runtime_error("error");
+                }
             }
 
             //mi metto in ascolto e attendo una modifica
             FileWatcher fw{directory, std::chrono::milliseconds(1000)};
-            fw.start([](std::string path_to_watch, FileStatus status) -> void {
+            fw.start([this](std::string path_to_watch, FileStatus status) -> void {
                 switch(status) {
                     case FileStatus::created:
-                    std::cout << "File created: " << path_to_watch << '\n';
-                    //TO DO interazione con server
+                    std::cout << "Created: " << path_to_watch << '\n';
+                    send(sock, std::string("created").c_str(), strlen( std::string("created").c_str())+1, 0);
+                    //dico al server che è stato creato un file e lo invio al server
+                    int ret = inviaFile(path_to_watch);
+                    if(ret < 0)
+                        throw std::runtime_error("errore invio file");
                     break;
                     case FileStatus::modified:
-                    std::cout << "File modified: " << path_to_watch << '\n';
-                    //TO DO interazione con server
+                    //dico al server che è stato modificato un file e lo invio al server
+                    std::cout << "Modified: " << path_to_watch << '\n';
+                    send(sock, std::string("modified").c_str(), strlen( std::string("modified").c_str())+1, 0);
+                    int ret = inviaFile(path_to_watch);
+                    if(ret < 0)
+                        throw std::runtime_error("errore invio file");
+                    
                     break;
                     case FileStatus::erased:
-                    std::cout << "File erased: " << path_to_watch << '\n';
-                    //TO DO interazione con server
+                    //dico al server che è stato modificato un file e lo invio al server
+                    send(sock, std::string("erased").c_str(), strlen( std::string("erased").c_str())+1, 0);
+                    std::cout << "Erased: " << path_to_watch << '\n';
+                    int ret = inviaFile(path_to_watch);
+                    if(ret < 0)
+                        throw std::runtime_error("errore invio file");
+                    
                     break;
                     default:
                     break;
@@ -99,5 +169,36 @@ void Client::handleConnection(){
         }  
     });
     controllaDirectory.detach();
+}
+
+int Client::inviaFile(std::string path_){
+    path p(path_);
+    if(is_directory(p)){//invio il nome della cartella al server
+         send(sock, p.relative_path().c_str(), strlen(p.relative_path().c_str())+1, 0);
+         recv(sock, &status, sizeof(int), 0);
+         return status;                     
+    }
+    if(is_regular_file(p)) //invio il file e il suo contenuto
+    {
+        struct stat obj;
+        int size, filehandle;
+        filehandle = open(p.relative_path().c_str(), O_RDONLY);
+        if(filehandle==-1){
+                return -1;
+        }else{
+            //invio nome file
+            send(sock, p.relative_path().c_str(), strlen(p.c_str())+1, 0);
+            //calcolo la sua dimensione e la invio al server
+            stat(p.c_str(), &obj); 
+            size = obj.st_size;
+            send(sock, &size, sizeof(int), 0);
+            //invio file
+            sendfile(sock, filehandle, NULL, size);
+            //ricevo un ack da server se è andato tutto a buon fine
+            recv(sock, &status, sizeof(int), 0);
+            return status;
+        }
+    }
+    return -2;
 }
 
--- client/Client.h
+++ client/Client.h
@@ -23,6 +23,7 @@ enum ClientStatus{
 
 class Client{
     int sock;
+    struct sockaddr_in saddr;
     ClientStatus status;
     static std::mutex m;
     static std::condition_variable modifica;
@@ -39,6 +40,6 @@ class Client{
     std::string readline(); //legge una riga da command line del client
 
     void close(); //chiude client
-
+    int inviaFile(std::string path_); //ivia file/directory al server, ritorna status
     void handleConnection(); //client in connessione con server e in ascolto per backup
 };
