ddf7e91e22be7387361aba904187e81dff56e82a
Yaku <lorenzo.deluca2407@gmail.com>
2020-07-30 20:18:02 +0200

    Implemented shared/Checksum

--- shared/Checksum.cpp
+++ shared/Checksum.cpp
@@ -1,1 +1,23 @@
 #include "Checksum.h"
+
+void Checksum::add(const char *str, int len) {
+    for (int i=0; i<len; i++) {
+        checksum = (checksum >> 1) + ((checksum & 1) << 7);
+        checksum += str[i];
+        checksum &= 0xff;
+    }
+}
+
+
+void Checksum::add(const std::string& str) {
+    add(str.c_str(), str.length());
+}
+
+void Checksum::reset() {
+    checksum = 0;
+}
+
+uint8_t Checksum::getChecksum() {
+    return checksum;
+}
+
--- shared/Checksum.h
+++ shared/Checksum.h
@@ -1,7 +1,20 @@
 #pragma once
 
-class Checksum {
+#include <cstdint>
+#include <string>
 
+/* Implementation of a 16 bits checksum using BSD checksum algorithm:
+ * en.wikipedia.org/wiki/BSD_checksum */
+
+class Checksum {
+private:
+    uint8_t checksum = 0;
+
+public:
+    void add(const char* str, int len);
+    void add(const std::string& str);
+    void reset();
+    uint8_t getChecksum();
 };
 
 
