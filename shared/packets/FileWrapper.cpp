8c1c385ee331f1ea0b9c5734e75dcd1bc687b831
s277922 <61941936+s277922@users.noreply.github.com>
2020-08-02 10:18:26 +0200

    Add files via upload
    change sizeof(data) in strlen(data)

---
+++ shared/packets/FileWrapper.cpp
@@ -0,0 +1,23 @@
+#include "FileWrapper.h"
+#include <iostream>
+#include <string.h>
+
+using namespace boost;
+using namespace serialization;
+
+FileWrapper::FileWrapper()
+{
+    
+}
+FileWrapper::FileWrapper(filesystem::path filePath, char *data, FileStatus status):
+    filePath(filePath), data(data), status(status)
+{
+    len = strlen(data);
+}
+
+void FileWrapper::print(){
+    std::cout <<"PATH: "<< filePath.relative_path().string() << std::endl;
+    std::cout <<"DATA:" << data << std::endl;
+    std::cout <<"SIZE:" << len << std::endl;
+}
+
