264bbdd952fbf828ba80a0e9ffc301799435cf88
Yaku <lorenzo.deluca2407@gmail.com>
2020-08-05 15:30:00 +0200

    Port on server can now be specified on runtime

--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,11 @@
 #include "Server.h"
 
-int main() {
-    Server s(5000);
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        std::cout<<"USAGE ./server PORT"<<std::endl;
+        exit(-1);
+    }
+
+    Server s(atoi(argv[1]));
     s.run();
 }
